b2772d034e3eb4f87021473f6aa0799a39a2d219
Roi lopez <64579967+roilopezz@users.noreply.github.com>
2022-01-19 16:44:14 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,59 +1,110 @@
-const { GoogleSpreadsheet } = require("google-spreadsheet");
 const express = require("express");
+const session = require("express-session");
 const app = express();
+const cors = require("cors");
+const { apiUrl } = require("./config.json");
+const userRouter = require("./routers/user");
+const postRouter = require("./routers/post");
+const conversationRouter = require("./routers/conversations");
+const friendsRouter = require("./routers/friends");
+const messagesRouter = require("./routers/messages");
+const http = require("http");
+const server = http.createServer(app);
+module.exports = { server };
+const socket = require("./models/socket");
+const multer = require("multer");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-const creds = require("./client_secret.json");
-const cors = require("cors");
-require("dotenv").config();
+app.use("/upload", express.static("upload"));
+
+// Loggers
+const morgan = require("morgan");
+app.use(morgan("dev"));
+
+// -- Session settings -- //
+const TWO_HOURS = 1000 * 60 * 60 * 2;
+const {
+  PORT = 3900,
+  NODE_ENV = "development",
+  SESS_NAME = "sid",
+  SESS_SECRET = "secretSecret",
+  SESS_LIFETIME = TWO_HOURS,
+} = process.env;
+
+const IN_PROD = NODE_ENV === "production";
+
+// app.enable("trust proxy", 1);
 
 app.use(
-  cors({
-    // origin: "http://localhost:3000",
-    origin: "https://tal-lopez.netlify.app",
-    credentials: true,
-    methods: ["POST"],
+  session({
+    name: SESS_NAME,
+    resave: false,
+    saveUninitialized: false,
+    secret: SESS_SECRET,
+    cookie: {
+      maxAge: SESS_LIFETIME,
+      sameSite: true,
+      // to enable cookies in heroku or another cloud you need to edit :
+      // sameSite: "none",
+      secure: IN_PROD,
+    },
   })
 );
 
-const { PORT = 1337, SHEET = "1FSg3QFEPVETCtWX-OOcrUFcmFDd9YDvq8WRzAohBfAM" } =
-  process.env;
+// -- enable cors -- //
+app.use(
+  cors({
+    origin: [apiUrl],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
 
-app.post("/", async (req, res) => {
-  if (!req.body.Name || !req.body.Phone || !req.body.Service) {
-    return res.status(404).send("name phone and service is required");
-  }
+// -- connect mongoDB -- //
+const mongoose = require("mongoose");
 
-  try {
-    const doc = new GoogleSpreadsheet(SHEET);
-    const connectSheet = await doc.useServiceAccountAuth(creds);
+mongoose
+  .connect("mongodb://localhost/social-net", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("connect to mongoDB");
+  })
+  .catch(() => {
+    console.log("the connection was fail");
+  });
 
-    const loadInfo = await doc.loadInfo(); // loads document properties and worksheets
-    const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
-    const addRow = await sheet.addRow({ ...req.body });
-    const saveRow = await sheet.saveUpdatedCells();
-
-    res.send("The data was sent successfully");
-  } catch (err) {
-    res.send(err).status(404);
-  }
-
-  //   (method) GoogleSpreadsheetWorksheet.addRow(values: {
-  //     [header: string]: string | number | boolean;
-  // } | (string | number | boolean)[], options?: {
-  //     raw: boolean;
-  //     insert: boolean;
-  // })
-
-  //   await doc.updateProperties({ title: 'renamed doc' });
-
-  //   const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
-  //   console.log(sheet.title);
-  //   console.log(sheet.rowCount);
-
-  //   // adding / removing sheets
-  //   const newSheet = await doc.addSheet({ title: 'hot new sheet!' });
-  //   await newSheet.delete();
+// -- settings upload image -- //
+const storage = multer.diskStorage({
+  destination: (req, res, cb) => {
+    cb(null, "upload/");
+  },
+  filename: (req, file, cb) => {
+    if (file.mimetype != "image/jpeg" && file.mimetype != "image/png") {
+      return null;
+    }
+    cb(null, `${Date.now()} - ${file.originalname}`);
+  },
 });
 
-app.listen(PORT, () => console.log(`running on ${PORT}`));
+const upload = multer({
+  storage,
+  limits: 1024 * 1024 * 2,
+});
+
+// routes //
+
+app.enable(socket);
+
+app.use("/", upload.single("image"), userRouter);
+app.use("/post", postRouter);
+app.use("/conversations", conversationRouter);
+app.use("/messages", messagesRouter);
+app.use("/friends", friendsRouter);
+
+server.listen(PORT, () => {
+  console.log(`listening on port : ${PORT}`);
+});
